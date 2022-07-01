fa2443af80eb73f943fbe8d25e340d141c4ebf32
Coral-Ann <coral.baker@outlook.com>
2022-07-01 11:03:13 +0100

    Added & tested leap years divisible by 100 but not 400 to be false.

--- lib/leap_years.rb
+++ lib/leap_years.rb
@@ -1,5 +1,7 @@
 def leap_year?(year)
   if year % 400 == 0
     true
+  elsif year % 100 == 0 && year % 400 != 0
+    false
   end
 end
--- spec/leap_years_spec.rb
+++ spec/leap_years_spec.rb
@@ -1,7 +1,12 @@
 require 'leap_years'
 
 describe '#leap_year?' do
+
   it 'should succesfully make years divisible by 400, true' do
     expect(leap_year?(2000)).to eq true
+  end
+
+  it 'should make years divisible by 100 but not 400, false' do
+    expect(leap_year?(1900)).to eq false
   end
 end
